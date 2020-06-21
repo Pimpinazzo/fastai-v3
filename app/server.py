68bf0d5d365ecd6d473d86c89ec7b8b86241902d
Pimpinazzo <tommaso.banzato@unipd.it>
2020-06-21 21:43:53 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1BnAHDmIVgxhh5Ae-PB8QdnqbX3pA4XKx'
+export_file_url = 'https://drive.google.com/file/d/1BnAHDmIVgxhh5Ae-PB8QdnqbX3pA4XKx/view?usp=sharing'
 export_file_name = 'export.pkl'
 
 classes = ['no_finding', 'interstitial_pattern', 'alveolar_pattern','mass','pneumothorax','pleural_effusion','cardiomegaly']
